6206baa28caa832b5752655bc80821eb4a237e64
Adrián Serrano <adrianno_3@hotmail.com>
2022-01-27 11:58:39 +0100

    folders structure v1

---
+++ assets/templates/footer.php
@@ -0,0 +1,7 @@
+<footer class="py-3 my-4 border-top">
+  <div class="footer-block">
+    <span class="text-center text-muted">© Assambler School Of Software Engineering 2020</span>
+  </div>
+</footer>
+</body>
+</html>
