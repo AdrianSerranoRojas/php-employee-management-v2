e949cd06022381d157f3536e21f0d9b3c0b04828
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2022-01-27 13:18:06 +0000

    etiquetas header&navbar

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../assets/css/main.css">
   <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@1,600&family=Cairo:wght@300;400&family=Kreon:wght@300;700&family=Lato:wght@300&family=Literata:ital@1&family=Oswald:wght@300&family=Titillium+Web:wght@300&display=swap" rel="stylesheet">
-  <title>Headers</title>
+  <title>EmployeeManagement</title>
 </head>
 <body>
 
--- assets/templates/navbar.php
+++ assets/templates/navbar.php
@@ -1,15 +1,14 @@
 <header class="main-header d-flex navbar navbar-expand-lg navbar-light bg-light">
-  <div class="d-flex d-flex-center main-header__container">
-    <img src="https://i2.wp.com/assemblerschool.com/wp-content/uploads/2020/11/LOGO-ORG.png?fit=479%2C131" alt="logo" class="main-header-logo">
-    <h3 class="main-header-title navbar-brand">Employees Management</h3>
-    <nav class="main-header__menu collapse navbar-collapse" id="navbarText">
-      <ul class="list-container d-flex navbar-nav mr-auto">
-        <li class="nav-item" id="tab-dashboard">
-          <a class="nav-link" href="./dashboard.php">Dashboard</a></li>
-        <li class="nav-item" id="tab-employee">
-          <a class="nav-link" href="./employee.php">New Employee</a></li>
-      </ul>
-    </nav>
-  </div>
-  </nav>
+    <div class="d-flex d-flex-center main-header__container">
+        <img src="https://i2.wp.com/assemblerschool.com/wp-content/uploads/2020/11/LOGO-ORG.png?fit=479%2C131" alt="logo" class="main-header-logo">
+        <h3 class="main-header-title navbar-brand">Employees Management</h3>
+        <nav class="main-header__menu collapse navbar-collapse" id="navbarText">
+            <ul class="list-container d-flex navbar-nav mr-auto">
+              <li class="nav-item" id="tab-dashboard">
+              <a class="nav-link" href="./dashboard.php">Dashboard</a></li>
+              <li class="nav-item" id="tab-employee">
+              <a class="nav-link" href="./employee.php">New Employee</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
